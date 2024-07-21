4bb838a36b31dc272da8045a1f6ac8d4af589166
sneaktricks <168283927+sneaktricks@users.noreply.github.com>
2024-07-21 18:07:39 +0300

    Add nginx

--- eventapp-ui/next.config.mjs
+++ eventapp-ui/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3001"],
+    },
+  },
 };
 
 export default nextConfig;
